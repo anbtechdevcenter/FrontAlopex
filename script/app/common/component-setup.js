e391b8070c4b09477da58810ef2005bc42f6cdfd
RESTNFEELSBOOK\RestNFeel <restnfeel@gmail.com>
2017-02-17 23:42:59 +0900

    기본소스 처리

---
+++ script/app/common/component-setup.js
@@ -0,0 +1,50 @@
+$a.page(function() {
+	  this.init = function(id, param) {
+		  //ToDo List 조회를 위해서 서버 정보 세팅
+		  //a.request.setup은 모든 reqeust에 공통으로 사용되므로 통신을 여러 번 사용하는 프로젝트에서는 각 페이지에서 정의하지 않고 common.js 같은 파일에 따로 작성하여 사용하시면 됩니다.
+		 $a.request.setup({
+				url : function(id, param) {
+		            return  'http://api.anbtech.net:8080/api/api'+ id+'.json'; // $a.request 서비스 ID가 적용될 것이다
+			    }, //서버 URL
+        dataType : 'json',
+				method : 'GET',
+				before : function(id, option) {
+          this.requestHeaders["Content-Type"] ="application/json; charset=UTF-8";
+					$('body').progress(); //progress bar 시작
+				},
+				after : function(res) {
+				    $('body').progress().remove();  //progress 종료
+				},
+
+				fail : function(res) {
+					alert('서버오류 입니다.');
+					$('body').progress().remove();  //progress 종료
+				},
+				error : function(err) {
+        //  console.error("[에러발생] ",err);
+
+					//alert('현재 네트상태를 확인하십시요.');
+					$('body').progress().remove();  //progress 종료
+				}
+			});
+
+
+      AlopexGrid.setup({
+      	autoColumnIndex: true,
+        numberingColumnFromZero: false,
+        rowSingleSelect : true,
+        rowClickSelect : true,
+        fitTableWidth: true,
+      	height: 300
+      });
+
+
+      $a.setup('popup', {
+        iframe: false
+      });
+
+
+
+    };
+
+});
---
+++ script/app/todoList.js
@@ -0,0 +1,115 @@
+$a.page(function() {
+	  this.init = function(id, param) {
+
+		//그리드 초기화
+		initGrid();
+
+		this.defineEvent();
+
+		setData();
+
+	  };
+
+
+		function setData(){
+			readBoard();
+		}
+
+		this.defineEvent = function(){
+			$("#btnSave").on("click", this.btnSave);
+			$('#btnDelete').on("click", this.btnDelete);
+			$("#btnSearch").on("click", this.btnSearch);
+		};
+
+		/**
+		* 게시판 저장
+		*/
+    this.btnSave = function(){
+			var check = confirm("저장하시겠습니까?");
+			if(check){
+				var data = $("#boardTb").getData();
+				console.log("data ", data);
+
+				$a.request("/board",{
+					method : 'post',
+					data : '#boardTb',
+					error : function(){
+						$("#boardTb").setData({boardTitle:'',boardContents:''});
+						readBoard();
+					}
+				});
+
+
+			}
+
+		};
+
+
+		this.btnSearch = function(){
+			readBoard();
+		};
+
+		this.btnDelete = function(){
+			var check = confirm("삭제하시겠습니까?");
+			if(check){
+
+				var selData = $("#grid_todolist").alopexGrid("dataGet", {_state : {selected:true}});
+
+				if(selData.length>0){
+					var id = selData[0].seqBoard;
+					$a.request("/board/"+id,{
+						method : 'delete',
+						error : function(){
+							readBoard();
+						}
+					});
+				}else{
+					alert("선택된 열이 없습니다.");
+				}
+
+
+			}
+		}
+
+
+		function readBoard(){
+			$a.request('/board', {
+		       success: function(res){
+						 $('#grid_todolist').alopexGrid("dataSet", res);
+					 }
+				});
+		}
+
+
+	  //그리드 초기화
+	  function initGrid() {
+			$('#grid_todolist').alopexGrid({
+				columnMapping : [
+					{
+						align : 'center',
+						selectorColumn : true,
+						title: '선택',
+						width : '20px',
+					},
+					{
+						align : 'center',
+						numberingColumn : true,
+						title: 'No',
+						width : '20px',
+					},
+						{
+							align : 'center',
+							key : 'boardTitle',
+							title: '제목',
+							width : '200px',
+						}, {
+							align : 'center',
+							key : 'boardContents',
+							title : '내용',
+							width : '300px'
+						}
+				]
+			});
+	  }
+
+});
