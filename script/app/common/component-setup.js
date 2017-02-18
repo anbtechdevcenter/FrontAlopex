10aafa88f57718fd74f109e242e233fcd105d1bb
RESTNFEELSBOOK\RestNFeel <restnfeel@gmail.com>
2017-02-18 10:28:45 +0900

    API URL 수정

--- script/app/common/component-setup.js
+++ script/app/common/component-setup.js
@@ -4,7 +4,7 @@ $a.page(function() {
 		  //a.request.setup은 모든 reqeust에 공통으로 사용되므로 통신을 여러 번 사용하는 프로젝트에서는 각 페이지에서 정의하지 않고 common.js 같은 파일에 따로 작성하여 사용하시면 됩니다.
 		 $a.request.setup({
 				url : function(id, param) {
-		            return  'http://api.anbtech.net:8080/api/api'+ id+'.json'; // $a.request 서비스 ID가 적용될 것이다
+		            return  'http://api.anbtech.net/api'+ id+'.json'; // $a.request 서비스 ID가 적용될 것이다
 			    }, //서버 URL
         dataType : 'json',
 				method : 'GET',
