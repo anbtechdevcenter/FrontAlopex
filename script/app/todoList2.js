81f114df379a0f5227107a0e9d6c3b1c8f9da458
DESKTOP-4S6K1GT\gil_h <gilhj0104@gmail.com>
2017-03-22 22:19:12 +0900

    1. 소스 들여쓰기 정리 (Tab length : 4) 2. 그리드 조회 추가 3. Datepicker 추가 4. 라디오 버튼 수정

--- script/app/todoList2.js
+++ script/app/todoList2.js
@@ -1,38 +1,48 @@
 $a.page(function() {
-	  this.init = function(id, param) {
-
+	this.init = function(id, param) {
 		//그리드 초기화
 		initGrid();
-	  };
-	  //그리드 초기화
-	  function initGrid() {
-			$('#grid_todolist2').alopexGrid({
-				columnMapping : [
-					{
-						align : 'center',
-						selectorColumn : true,
-						title: '선택',
-						width : '20px',
-					},
-					{
-						align : 'center',
-						numberingColumn : true,
-						title: 'No',
-						width : '20px',
-					},
-						{
-							align : 'center',
-							key : 'boardTitle',
-							title: '제목',
-							width : '200px',
-						}, {
-							align : 'center',
-							key : 'boardContents',
-							title : '내용',
-							width : '300px'
-						}
-				]
-			});
-	  }
+		readBoard();
+	};
+
+	// 조회
+	function readBoard(){
+		ANBTX.R('/board',
+			function(res){
+				$('#grid_todolist2').alopexGrid("dataSet", res);
+			}
+		);
+	}
+	//그리드 초기화
+	function initGrid() {
+		$('#grid_todolist2').alopexGrid({
+			columnMapping : [
+				{
+					align : 'center',
+					selectorColumn : true,
+					title: '선택',
+					width : '20px',
+				},
+				{
+					align : 'center',
+					numberingColumn : true,
+					title: 'No',
+					width : '20px',
+				},
+				{
+					align : 'center',
+					key : 'boardTitle',
+					title: '제목',
+					width : '200px',
+				},
+				{
+					align : 'center',
+					key : 'boardContents',
+					title : '내용',
+					width : '300px'
+				}
+			]
+		});
+	}
 
 });
